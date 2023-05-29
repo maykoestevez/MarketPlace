ec115abe9c3670f827911c26a1b4a25fb9d747a5
maykoestevez <MaykoYefree.EstevezPena@iwgplc.com>
2023-05-28 21:17:54 -0400

    Updating to use docker

---
+++ MarketPlace.api/MarketPlace.Infrastructure/Database/20230523103842_AddingSeedData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MarketPlace.MarketPlace.Infrastructure.Database
+{
+    /// <inheritdoc />
+    public partial class AddingSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments");
+
+            migrationBuilder.RenameTable(
+                name: "Payments",
+                newName: "Orders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "CreatedDate", "Description", "IsActive", "LastModifiedDate", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { -2, "Tech", new DateTime(2023, 5, 23, 6, 38, 42, 274, DateTimeKind.Local).AddTicks(8180), "Brand new Lenovo PC Brand #12", true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Laptop Dell", 1200.0, 0 },
+                    { -1, "Tech", new DateTime(2023, 5, 23, 6, 38, 42, 274, DateTimeKind.Local).AddTicks(8140), "Brand new Lenovo PC Brand #12", true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Laptop Lenovo", 1200.0, 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: -2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: -1);
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Payments");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payments",
+                table: "Payments",
+                column: "Id");
+        }
+    }
+}
